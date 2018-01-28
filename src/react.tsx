94f8fbf0ad65e0871185325b0495de2ebc7e44fe
Boris Cherny <boris@performancejs.com>
2018-01-28 10:42:38 -0800

    add flow

--- src/react.tsx
+++ src/react.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react'
+import { ComponentClass } from 'react'
 import { IDisposable } from 'rx'
 import { Store } from './'
-import { ComponentClass } from 'react';
 
-type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
-type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] }
-type Overwrite<T, U> = { [P in Diff<keyof T, keyof U>]: T[P] } & U
+export type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
+export type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] }
 
 export function connect<Actions extends object>(store: Store<Actions>) {
   return (...listenOn: (keyof Actions)[]) => {
