eb5baf93544a7fed978c635108fe25439dd41b52
IzumiSy <beetle-noise@gmx.com>
2018-02-12 04:25:34 +0000

    Reverted utils.ts to declare isImmutable method again and use it in equals method

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { is } from 'immutable';
+import is from 'immutable-is';
 
 /**
  * TODO: Avoid diffing by passing individual values into a React component
@@ -7,7 +7,17 @@ import { is } from 'immutable';
  * handle diffing for us.
  */
 export function equals<T>(a: T, b: T): boolean {
-  return is(a, b)
+  if (isImmutable(a) && isImmutable(b)) {
+    return is(a, b)
+  }
+  return a === b;
+}
+
+export function isImmutable(obj: any): boolean {
+  return !!obj && typeof obj === 'object' && (
+    '@@__IMMUTABLE_ITERABLE__@@' in obj ||
+    '@@__IMMUTABLE_RECORD__@@' in obj
+  )
 }
 
 export function getDisplayName<T>(Component: React.ComponentType<T>): string {
