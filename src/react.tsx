fd77e7117beac6f6540a46b4718a807d22a7506d
8th713 <8th713@gmail.com>
2018-03-10 12:30:03 +0900

    move comparison operation from the connect to the store

--- src/react.tsx
+++ src/react.tsx
@@ -4,41 +4,34 @@ import { IDisposable } from 'rx'
 import { Store } from './'
 import { equals, getDisplayName } from './utils'
 
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
+export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
+  { [P in U]: never } & { [x: string]: never })[T]
 export type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] }
 
 export function connect<Actions extends object>(store: Store<Actions>) {
   return (...listenOn: (keyof Actions)[]) => {
-    return function <
+    return function<
       Props,
-      PropsWithStore extends { store: Store<Actions> } & Props = { store: Store<Actions> } & Props
+      PropsWithStore extends { store: Store<Actions> } & Props = {
+        store: Store<Actions>
+      } & Props
     >(
       Component: React.ComponentType<PropsWithStore>
     ): React.ComponentClass<Omit<PropsWithStore, 'store'>> {
+      let state: IDisposable[]
 
-      let state: IDisposable[][]
-
-      let Class: ComponentClass<Omit<PropsWithStore, 'store'>> = class extends React.Component<Omit<PropsWithStore, 'store'>> {
+      let Class: ComponentClass<
+        Omit<PropsWithStore, 'store'>
+      > = class extends React.Component<Omit<PropsWithStore, 'store'>> {
         componentDidMount() {
           state = listenOn.map(key => {
-            let ignore = false
-            return [
-              store.before(key).subscribe(({ previousValue, value }) => {
-                if (equals(previousValue, value)) {
-                  return ignore = true
-                }
-              }),
-              store.on(key).subscribe(() => {
-                if (ignore) {
-                  return ignore = false
-                }
-                this.forceUpdate()
-              })
-            ]
+            return store.on(key).subscribe(() => {
+              this.forceUpdate()
+            })
           })
         }
         componentWillUnmount() {
-          state.forEach(_ => _.forEach(_ => _.dispose()))
+          state.forEach(_ => _.dispose())
         }
         render() {
           return <Component {...this.props} store={store} />
