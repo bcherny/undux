afc3a7efaa16d47b89762aadfa82ce43c1d00382
Boris Cherny <boris@performancejs.com>
2018-07-16 17:11:18 -0700

    Revert "Merge pull request #44 from anarelion/only-respond-to-updated-fields"
    This reverts commit 2ac3dc8ed1b12d20ed97e5f4def7cecdca4f01ed, reversing
    changes made to e5e294bd35713bac31eb00ff0872875ee0900898.

--- src/react.tsx
+++ src/react.tsx
@@ -6,31 +6,6 @@ import { equals, getDisplayName, keys, mapValues, some } from './utils'
 
 export type Diff<T, U> = Pick<T, Exclude<keyof T, keyof U>>
 
-class StoreSnapshotWrapper<StoreState extends object> implements Store<StoreState> {
-  constructor(
-    private snapshot: StoreSnapshot<StoreState>,
-    private callbackOnGet: (key: keyof StoreState) => void,
-    private callbackOnGetAll: () => void
-  ) { }
-  get<K extends keyof StoreState>(key: K) {
-    this.callbackOnGet(key)
-    return this.snapshot.get(key)
-  }
-  set<K extends keyof StoreState>(key: K) {
-    return this.snapshot.set(key)
-  }
-  on<K extends keyof StoreState>(key: K) {
-    return this.snapshot.on(key)
-  }
-  onAll() {
-    return this.snapshot.onAll()
-  }
-  getState() {
-    this.callbackOnGetAll()
-    return this.snapshot.getState()
-  }
-}
-
 export function connect<StoreState extends object>(store: StoreDefinition<StoreState>) {
   return function <
     Props,
@@ -39,61 +14,24 @@ export function connect<StoreState extends object>(store: StoreDefinition<StoreS
     Component: React.ComponentType<PropsWithStore>
   ): React.ComponentClass<Diff<PropsWithStore, { store: Store<StoreState> }>> {
 
-    type SubscriptionKeys = keyof StoreState | '<all>'
-
     type State = {
-      store: StoreSnapshotWrapper<StoreState>,
-      subscriptions: Map<SubscriptionKeys, Subscription>
+      store: StoreSnapshot<StoreState>
+      subscription: Subscription
     }
 
     return class extends React.Component<Diff<PropsWithStore, { store: Store<StoreState> }>, State> {
       static displayName = `withStore(${getDisplayName(Component)})`
-
-      private onGet = (field: keyof StoreState) => {
-        if (this.state.subscriptions.has(field) || this.state.subscriptions.has('<all>')) {
-          return
-        }
-        const newSubscriptions = new Map(this.state.subscriptions)
-        newSubscriptions.set(
-          field,
-          store.on(field).subscribe(value => {
-            if (equals(value, this.state.store.get(field))) {
-              return
-            }
-            this.setState({
-              store: new StoreSnapshotWrapper(store.getCurrentSnapshot(), this.onGet, this.onGetAll)
-            })
-          })
-        )
-        this.setState({ subscriptions: newSubscriptions })
-      }
-
-      private onGetAll = () => {
-        if (this.state.subscriptions.has('<all>')) {
-          return
-        }
-        const newSubscriptions = new Map()
-        newSubscriptions.set(
-          '<all>',
-          store.onAll().subscribe(({ previousValue, value }) => {
-            if (equals(previousValue, value)) {
-              return
-            }
-            this.setState({
-              store: new StoreSnapshotWrapper(store.getCurrentSnapshot(), this.onGet, this.onGetAll)
-            })
-          })
-        )
-        this.state.subscriptions.forEach(_ => _.unsubscribe())
-        this.setState({ subscriptions: newSubscriptions })
-      }
-
       state = {
-        store: new StoreSnapshotWrapper(store.getCurrentSnapshot(), this.onGet, this.onGetAll),
-        subscriptions: new Map()
+        store: store.getCurrentSnapshot(),
+        subscription: store.onAll().subscribe(({ previousValue, value }) => {
+          if (equals(previousValue, value)) {
+            return false
+          }
+          this.setState({ store: store.getCurrentSnapshot() })
+        })
       }
       componentWillUnmount() {
-        this.state.subscriptions.forEach(_ => _.unsubscribe())
+        this.state.subscription.unsubscribe()
       }
       shouldComponentUpdate(props: Readonly<Diff<PropsWithStore, { store: Store<StoreState> }>>, state: State) {
         return state.store !== this.state.store
