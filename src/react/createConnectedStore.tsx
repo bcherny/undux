b5a1cf067345b0232a86d7b516583d111523233e
Boris Cherny <boris@performancejs.com>
2018-09-03 19:03:24 -0700

    implement connect in terms of connectAs, createConnectedStore in terms of createConnectedStoreAs

--- src/react/createConnectedStore.tsx
+++ src/react/createConnectedStore.tsx
@@ -1,14 +1,6 @@
 import * as React from 'react'
-import { Subscription } from 'rxjs-observable'
-import {
-  createStore,
-  Effects,
-  Store,
-  StoreDefinition,
-  StoreSnapshot,
-  StoreSnapshotWrapper
-} from '..'
-import { Diff, equals, getDisplayName, some } from '../utils'
+import { createConnectedStoreAs, Effects, Store } from '..'
+import { Diff } from '../utils'
 
 export type Connect<State extends object> = {
   Container: React.ComponentType<ContainerProps<State>>
@@ -26,146 +18,19 @@ export function createConnectedStore<State extends object>(
   initialState: State,
   effects?: Effects<State>
 ): Connect<State> {
-  let Context = React.createContext({ __MISSING_PROVIDER__: true } as any)
-
-  type ContainerState = {
-    storeSnapshot: StoreSnapshot<State>
-  }
-
-  class Container extends React.Component<
-    ContainerProps<State>,
-    ContainerState
-  > {
-    subscription: Subscription
-    storeDefinition: StoreDefinition<State>
-    constructor(props: ContainerProps<State>) {
-      super(props)
-
-      // Create store definition from initial state
-      let state = props.initialState || initialState
-      this.storeDefinition = createStore(state)
-
-      // Apply effects?
-      let fx = props.effects || effects
-      if (fx) {
-        fx(this.storeDefinition)
+  let { Container, withStores } = createConnectedStoreAs(
+    {
+      store: initialState
+    },
+    s => {
+      if (effects) {
+        effects(s.store)
       }
-
-      this.state = {
-        storeSnapshot: this.storeDefinition.getCurrentSnapshot()
-      }
-
-      this.subscription = this.storeDefinition.onAll().subscribe(() =>
-        this.setState({
-          storeSnapshot: this.storeDefinition.getCurrentSnapshot()
-        })
-      ) as any
+      return s
     }
-    componentWillUnmount() {
-      this.subscription.unsubscribe()
-      // Let the state get GC'd.
-      // TODO: Find a more elegant way to do this.
-      ; (this.storeDefinition as any).storeSnapshot = null
-      ; (this as any).storeDefinition = null
-    }
-    render() {
-      return (
-        <Context.Provider value={this.state.storeSnapshot}>
-          {this.props.children}
-        </Context.Provider>
-      )
-    }
-  }
-
-  let Consumer = (props: {
-    children: (store: StoreSnapshot<State>) => JSX.Element
-    displayName: string
-  }) => (
-    <Context.Consumer>
-      {store => {
-        if (!isInitialized(store)) {
-          throw Error(
-            `[Undux] Component "${
-              props.displayName
-            }" does not seem to be nested in an Undux <Container>. To fix this error, be sure to render the component in the <Container>...</Container> component that you got back from calling createConnectedStore().`
-          )
-        }
-        return props.children(store)
-      }}
-    </Context.Consumer>
   )
-
-  function withStore<
-    Props extends { store: Store<State> },
-    PropsWithoutStore = Diff<Props, { store: Store<State> }>
-  >(
-    Component: React.ComponentType<Props>
-  ): React.ComponentType<PropsWithoutStore> {
-    let displayName = getDisplayName(Component)
-
-    type _Props = {
-      props: object
-      storeSnapshot: StoreSnapshot<State>
-    }
-
-    class SnapshotComponent extends React.Component<_Props> {
-      private isSubscribedToAllFields = false
-      // https://jsperf.com/set-membership-vs-object-key-lookup
-      private subscribedFields: Partial<Record<keyof State, true>> = {}
-      shouldComponentUpdate(nextProps: _Props) {
-        if (this.isSubscribedToAllFields) {
-          return true
-        }
-        return (
-          some(
-            this.subscribedFields,
-            (_, k) =>
-              !equals(
-                nextProps.storeSnapshot.get(k),
-                this.props.storeSnapshot.get(k)
-              )
-          ) || some(nextProps.props, (v, k) => !equals(v, this.props.props[k]))
-        )
-      }
-      onGetOrSet = (key: keyof State) => {
-        if (this.isSubscribedToAllFields) {
-          return
-        }
-        this.subscribedFields[key] = true
-      }
-      onGetAll = () => {
-        this.isSubscribedToAllFields = true
-        this.subscribedFields = {}
-      }
-      render() {
-        let wrapper = new StoreSnapshotWrapper(
-          this.props.storeSnapshot,
-          this.onGetOrSet,
-          this.onGetAll
-        )
-        return <Component store={wrapper} {...this.props.props} />
-      }
-    }
-
-    let f: React.StatelessComponent<PropsWithoutStore> = props => (
-      <Consumer displayName={displayName}>
-        {storeSnapshot => (
-          <SnapshotComponent storeSnapshot={storeSnapshot} props={props} />
-        )}
-      </Consumer>
-    )
-    f.displayName = `withStore(${displayName})`
-    return f
-  }
-
   return {
-    Container,
-    withStore
+    Container: Container as any,
+    withStore: withStores
   }
-}
-
-function isInitialized<State extends object>(
-  store: StoreSnapshot<State> | { __MISSING_PROVIDER__: true }
-) {
-  return !('__MISSING_PROVIDER__' in store)
 }
